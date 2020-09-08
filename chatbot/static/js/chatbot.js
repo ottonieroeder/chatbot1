bd311f3ea18a1c69b317f906448432a82c548eb3
Norma Driske <norma@entwicklerheld.de>
2020-09-08 23:22:37 +0200

    Prettify the chat

--- chatbot/static/js/chatbot.js
+++ chatbot/static/js/chatbot.js
@@ -1,28 +1,28 @@
 const addUserInputToLog = (inputValue) => {
     const userContainer = document.querySelector("#chat");
-    var containerRow = document.createElement("div")
-    var containerCol = document.createElement("div")
-    var tag = document.createElement("p");
-    var text = document.createTextNode(inputValue);
-    containerRow.classList.add("row")
-    containerRow.classList.add("no-gutters")
-    containerRow.classList.add("justify-content-end")
-    containerCol.classList.add("col-4")
+    const containerRow = document.createElement("div")
+    const containerCol = document.createElement("div")
+    const tag = document.createElement("p");
+    const text = document.createTextNode(inputValue);
+    containerRow.classList.add("row");
+    containerRow.classList.add("no-gutters");
+    containerRow.classList.add("justify-content-end");
+    containerCol.classList.add("col-4");
     tag.classList.add("bubble-right");
 
     tag.appendChild(text);
     containerCol.appendChild(tag);
     containerRow.appendChild(containerCol);
     userContainer.appendChild(containerRow);
-    console.log(userContainer)
 }
 
 const addBotAnswerToLog = (botResponse) => {
     const botContainer = document.querySelector("#chat");
-    var tag = document.createElement("p");
-    var containerRow = document.createElement("div")
-    var containerCol = document.createElement("div")
-    var text = document.createTextNode(botResponse);
+    const tag = document.createElement("p");
+    const containerRow = document.createElement("div");
+    const containerCol = document.createElement("div");
+    const text = document.createTextNode(botResponse);
+
     containerRow.classList.add("row");
     containerCol.classList.add("col-4");
     tag.classList.add("bubble-left");
@@ -37,11 +37,17 @@ const emptyInputField = () => {
     document.querySelector("#user-input").value="";
 }
 
+const scrollToBottom = () => {
+    const chat = document.querySelector("#chat");
+    chat.scrollTop = chat.scrollHeight;
+}
+
 async function sendUserInput(event) {
     event.preventDefault();
     const inputValue = document.querySelector("#user-input").value;
-    emptyInputField()
-    addUserInputToLog(inputValue)
+    emptyInputField();
+    addUserInputToLog(inputValue);
+    scrollToBottom();
     const url = window.location.origin + chatterbotUrl;
     await fetch(url, {
         method: 'POST',
@@ -54,7 +60,8 @@ async function sendUserInput(event) {
         referrerPolicy: 'no-referrer',
         body: JSON.stringify({'text': inputValue})
     }).then(response => response.json()).then(data => {
-        addBotAnswerToLog(data.text)
+        addBotAnswerToLog(data.text);
+        scrollToBottom();
     });
 }
 
