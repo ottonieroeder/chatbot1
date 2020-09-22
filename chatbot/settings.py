737ec1aba59fe66a3f234387b976a9cf1eb091ef
Norma Driske <norma@entwicklerheld.de>
2020-09-22 11:12:57 +0200

    Add environment variables

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -1,18 +1,11 @@
 import os
 from pathlib import Path
 
-# Build paths inside the chatbot like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "eda_%24rss6!(1rtxm1mvpyc%b!ao^my)^u%#7atsrf3em71ac")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "eda_%24rss6!(1rtxm1mvpyc%b!ao^my)^u%#7atsrf3em71ac"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', '') != 'False'
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
--- chatbot/wsgi.py
+++ chatbot/wsgi.py
@@ -1,6 +1,12 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+from django.conf import settings
+
+if not settings.DEBUG:
+    project_folder = os.path.expanduser('~/isabot.pythonanywhere.com')
+    load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot.settings")
 
