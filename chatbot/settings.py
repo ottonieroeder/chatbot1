b6133f1be03465c83d0973ba9c097c186c3d579d
Norma Driske <norma@entwicklerheld.de>
2020-09-25 01:15:42 +0200

    Change bot settings

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -31,12 +31,9 @@ CORPUS_DIR = str(BASE_DIR.joinpath("chatbot/corpus"))
 
 CHATTERBOT = {
     "name": "Isabot",
-    "storage_adapter": "chatterbot.storage.SQLStorageAdapter",
     'logic_adapters': [
         {
             'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'I am sorry, but I do not understand, try it again!',
-            'maximum_similarity_threshold': 0.1
         },
     ],
     'trainer': 'chatterbot.trainers.ChatterBotCorpusTrainer',
