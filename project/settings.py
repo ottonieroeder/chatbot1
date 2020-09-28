0415fd1b4248c0b2115f403b87b159004abb8835
Norma Driske <hello@normade.dev>
2020-09-28 23:40:50 +0200

    Update readme

--- project/settings.py
+++ project/settings.py
@@ -125,9 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-# if not DEBUG:
-#     MEDIA_URL = "/media/"
-#     STATIC_ROOT = os.path.join(BASE_DIR, "../chatbot/static")
-#     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if not DEBUG:
+    MEDIA_URL = "/media/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "../chatbot/static")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
