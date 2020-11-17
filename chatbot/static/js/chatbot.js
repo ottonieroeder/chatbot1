cd8cb6fe1a18d09d113d467fd7ed9628ca639f33
Norma Driske <hello@normade.dev>
2020-11-17 14:57:53 +0100

    Make video modal dragable

--- chatbot/static/js/chatbot.js
+++ chatbot/static/js/chatbot.js
@@ -169,6 +169,40 @@ const loadVideo = () => {
     }
 }
 
+const dragElement = (elmnt) => {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", loadVideo());
 
 window.onload = (event) => {
@@ -176,4 +210,6 @@ window.onload = (event) => {
     addClickEventListenerToSessionButton();
     addClickEventListenerToVideoContainer();
     startBotCommunication();
+    dragElement(document.getElementById("isa-video-modal"));
 };
+
